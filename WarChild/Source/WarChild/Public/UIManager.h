8d198cb1f98272792f7f23e7f338d499ec8437be
Sweeny <44808412+MikeSweeny@users.noreply.github.com>
2020-03-25 22:34:14 -0400

    Added UIManager and GameEventManager interfaces.
    I figured out how to do interfaces, they are stupid simple. Made both our GameEventManager and our UIManager using interfaces and am discussing the merits of converting our MissionTracker to an interface as well to handle some quest events.

---
+++ WarChild/Source/WarChild/Public/UIManager.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "UIManager.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UUIManager : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class WARCHILD_API IUIManager
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	// CLASSES THAT WANT TO USE THIS EVENT MANAGER:
+	// Class must inherit from IUIManager
+	// Classes must publicly define all events (even by leaving them empty) or it will cause errors.
+	void OpenNavigation();
+	void CloseNavigation();
+	void OpenArsenal();
+	void CloseArsenal();
+	void OpenExtraction();
+	void CloseExctraction();
+
+};
