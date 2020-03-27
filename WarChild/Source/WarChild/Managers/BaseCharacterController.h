446267c4ac4b487cc75173ac101bfb4cac4508d2
StevenCoverdale <S.coverdale@hotmail.com>
2020-03-27 15:25:47 -0400

    1

--- WarChild/Source/WarChild/Managers/BaseCharacterController.h
+++ WarChild/Source/WarChild/Managers/BaseCharacterController.h
@@ -8,11 +8,10 @@
 #include "BaseCharacterController.generated.h"
 
 
-	DECLARE_DYNAMIC_MULTICAST_DELEGATE_SixParams(FBeginOverlapSignature, UPrimitiveComponent*, OverlappedComponent, AActor*, OtherActor,
-	UPrimitiveComponent*, OtherComp, int32, OtherBodyIndex, bool, bFromSweep,
-		const FHitResult, &SweepResult);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_SixParams(FBeginOverlapSignature1, UPrimitiveComponent*, OverlappedComponent, AActor*, OtherActor, UPrimitiveComponent*, OtherComp, int32, OtherBodyIndex, bool, bFromSweep, const FHitResult&, SweepResult);
 
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FRamaMeleeHitSignature, class AActor*, HitActor);
 /**
  * 
  */
@@ -33,7 +32,10 @@ private:
 
 
 	UPROPERTY()
-	FBeginOverlapSignature BegunOverlap;
+	FBeginOverlapSignature1 BegunOverlap;
+
+	UPROPERTY()
+	FRamaMeleeHitSignature onHit;
 
 
 public:
