d14af963aa114c459c4c91fd1f5af84b5ee00144
Sweeny <44808412+MikeSweeny@users.noreply.github.com>
2020-04-01 15:33:58 -0400

    Starting buttons and menus.

---
+++ WarChild/Source/WarChild/UI/Menus/BaseMenu.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseMenu.h"
+
+UBaseMenu::UBaseMenu()
+{
+
+}
+
+UBaseMenu::~UBaseMenu()
+{
+}
---
+++ WarChild/Source/WarChild/UI/Menus/BaseMenu.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BaseMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARCHILD_API UBaseMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+private:
+
+
+public:
+
+	UBaseMenu();
+	~UBaseMenu();
+
+	//virtual void Tick(FGeometry MyGeometry, float InDeltaTime) override;
+
+};
